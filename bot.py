9aa50bee98db46bff6e4115fd2a1b0d9f5dacf3b
javiersanpan <javiersanpan@outlook.com>
2023-07-13 12:41:06 -0600

    python file with recipe search

---
+++ bot.py
@@ -0,0 +1,6 @@
+import search_recipe_by_ingredient
+
+search_recipe_by_ingredient.search_recipe_by_ingredient("kilogramo", "recipe_results.txt")
+
+while True: 
+    print("¡Hola! escribe un ingrediente y te daré recetas mexicanas basados en eso")
